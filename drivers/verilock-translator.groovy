545a4f0ef1bea2819750ab71f81641e3e4e8c82b
dcmeglio <dmeglio@gmail.com>
2020-05-15 16:09:02 -0400

    Update verilock-translator.groovy

--- drivers/verilock-translator.groovy
+++ drivers/verilock-translator.groovy
@@ -18,7 +18,6 @@ metadata {
         capability "Contact Sensor"
 		capability "Configuration"
 		capability "Sensor"
-		capability "Zw Multichannel"
         
         fingerprint deviceId: "0x414E", inClusters: "0x5E,0x86,0x70,0x8E,0x85,0x59,0x7A,0x71,0x22,0x5A,0x73,0x72,0x60"
 	}
@@ -49,7 +48,6 @@ def zwaveEvent(hubitat.zwave.commands.wakeupv1.WakeUpNotification cmd) {
 
 def zwaveEvent(hubitat.zwave.commands.notificationv3.NotificationReport cmd, ep = null)
 {
-log.debug "Notification: ${cmd}"
     def evtName
     def evtValue
     switch(cmd.event){
@@ -77,7 +75,7 @@ log.debug "Notification: ${cmd}"
                 [completedSetup: true, label: "${device.displayName} Window ${ep}",
                 isComponent: false, componentName: "ep$ep", componentLabel: "Window $ep"])
     }
-log.debug "Got here ${evtName} ${evtVaue}"
+
     childDevice.sendEvent(name: evtName, value: evtValue)
         
     def allLocked = true
@@ -99,16 +97,6 @@ log.debug "Got here ${evtName} ${evtVaue}"
     }
 }
 
-private List loadEndpointInfo() {
-	if (state.endpointInfo) {
-		state.endpointInfo
-	} else if (device.currentValue("epInfo")) {
-		fromJson(device.currentValue("epInfo"))
-	} else {
-		[]
-	}
-}
-
 def updated() {
     childDevices.each {
         if (it.label == "${state.oldLabel} ${channelNumber(it.deviceNetworkId)}") {
@@ -122,40 +110,6 @@ def updated() {
 private channelNumber(String dni) {
 	dni.split("-ep")[-1] as Integer
 }
-
-def zwaveEvent(hubitat.zwave.commands.multichannelv3.MultiChannelEndPointReport cmd) {
-log.debug "endpointreport: ${cmd}"
-	updateDataValue("endpoints", cmd.endPoints.toString())
-	if (!state.endpointInfo) {
-		state.endpointInfo = loadEndpointInfo()
-	}
-	if (state.endpointInfo.size() > cmd.endPoints) {
-		cmd.endpointInfo
-	}
-	state.endpointInfo = [null] * cmd.endPoints
-	//response(zwave.associationV2.associationGroupingsGet())
-	[ createEvent(name: "epInfo", value: groovy.json.JsonOutput.toJson(state.endpointInfo), displayed: false, descriptionText:""),
-	  response(zwave.multiChannelV3.multiChannelCapabilityGet(endPoint: 1)) ]
-}
-
-/*def zwaveEvent(hubitat.zwave.commands.multichannelv3.MultiChannelCapabilityReport cmd) {
-log.debug "cmd: " + cmd
-log.debug "fmt: " + cmd.format()
-	def result = []
-	def cmds = []
-	if(!state.endpointInfo) state.endpointInfo = []
-	if (cmd.format().size() > 6) {
-		state.endpointInfo[cmd.endPoint - 1] = cmd.format()[6..-1]
-		if (cmd.endPoint < getDataValue("endpoints").toInteger()) {
-			cmds = zwave.multiChannelV3.multiChannelCapabilityGet(endPoint: cmd.endPoint + 1).format()
-		} else {
-			logDebug "endpointInfo: ${state.endpointInfo.inspect()}"
-		}
-		result << createEvent(name: "epInfo", value: groovy.json.JsonOutput.toJson(state.endpointInfo), displayed: false, descriptionText:"")
-		if(cmds) result << response(cmds)
-		result
-	}
-}*/
 
 def zwaveEvent(hubitat.zwave.commands.associationv2.AssociationGroupingsReport cmd) {
 	state.groups = cmd.supportedGroupings
@@ -172,29 +126,13 @@ def zwaveEvent(hubitat.zwave.commands.associationgrpinfov1.AssociationGroupInfoR
 	cmds
 }
 
-// Multi-channel event from the device. Version 1 of Command Class
-def zwaveEvent(hubitat.zwave.commands.multiinstancev1.MultiInstanceCmdEncap cmd) {
-	log.debug ("DEBUG  multiinstancev1.MultiInstanceCmdEncap cmd=${cmd}")
-	zwaveEventMultiCmdEncap(cmd)
-}
 
 def zwaveEvent(hubitat.zwave.commands.multichannelv3.MultiChannelCmdEncap cmd) {
 	def encapsulatedCommand = cmd.encapsulatedCommand([0x32: 3, 0x25: 1, 0x20: 1])
-	log.debug "cmd: ${cmd}"
 	if (encapsulatedCommand) {
-		log.debug "encapsulatedCommand: ${encapsulatedCommand}"
-		if (state.enabledEndpoints.find { it == cmd.sourceEndPoint }) {
-			log.debug "found"
-			def formatCmd = ([cmd.commandClass, cmd.command] + cmd.parameter).collect{ String.format("%02X", it) }.join()
-			log.debug "formatCmd ${formatCmd}"
-			createEvent(name: "epEvent", value: "$cmd.sourceEndPoint:$formatCmd", isStateChange: true, displayed: false, descriptionText: "(fwd to ep $cmd.sourceEndPoint)")
-		} else {
-			log.debug "not found"
-			zwaveEvent(encapsulatedCommand, cmd.sourceEndPoint as Integer)
-		}
+		zwaveEvent(encapsulatedCommand, cmd.sourceEndPoint as Integer)
 	}
 }
-
 
 def zwaveEvent(hubitat.zwave.Command cmd) {
 	createEvent(descriptionText: "$device.displayName: $cmd", isStateChange: true)
@@ -218,17 +156,6 @@ private commands(commands, delay=200) {
 	delayBetween(commands.collect{ command(it) }, delay)
 }
 
-private encap(cmd, endpoint) {
-	if (endpoint) {
-		command(zwave.multiChannelV3.multiChannelCmdEncap(destinationEndPoint:endpoint).encapsulate(cmd))
-	} else {
-		command(cmd)
-	}
-}
-
-private encapWithDelay(commands, endpoint, delay=200) {
-	delayBetween(commands.collect{ encap(it, endpoint) }, delay)
-}
 
 def logDebug(msg) {
     if (settings?.debugOutput) {
